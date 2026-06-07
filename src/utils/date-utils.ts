119ea29c6abd262c28b2e233f634fd533e707c5d
Evandro Passaia <evandrobaraka@gmail.com>
2026-06-07 15:37:16 -0300

    Move API files into its subdirectory

--- src/utils/date-utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converte uma string de data no formato DD/MM/YYYY para um objeto Date.
- * @param dateStr String de data no formato DD/MM/YYYY
- * @returns Objeto Date correspondente
- */
-export const parseDateBR = (dateStr: string): Date => {
-  const [day, month, year] = dateStr.split("/").map(Number);
-  return new Date(year, month - 1, day);
-};
