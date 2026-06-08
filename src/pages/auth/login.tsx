be09ea9fec14330d17d6d2d61f37a40b0ffaf31f
Evandro Passaia <evandrobaraka@gmail.com>
2026-06-07 22:59:07 -0300

    Cleanup root directory leftovers after merge

--- src/pages/auth/login.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { LoginForm } from "../../components/LoginForm";
-
-export const Route = createFileRoute("/auth/login")({
-    component: () => <LoginForm />,
-});
